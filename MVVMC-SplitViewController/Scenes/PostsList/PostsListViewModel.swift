8ed97a88d517839a818e6eabfaf2b8b047add918
mgacy <matt@mgacy.com>
2017-12-28 14:40:08 -0700

    Add basic Posts scene
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ MVVMC-SplitViewController/Scenes/PostsList/PostsListViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  PostsListViewModel.swift
+//  MVVMC-SplitViewController
+//
+//  Created by Mathew Gacy on 12/27/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+
+final class PostsListViewModel: AttachableViewModelType {
+
+    struct Dependency {
+        let client: APIClient
+    }
+
+    struct Bindings {
+        let fetchTrigger: Driver<Void>
+        let selection: Driver<IndexPath>
+    }
+
+    // Properties
+    let fetching: Driver<Bool>
+    let posts: Driver<[Post]>
+    let selectedPost: Driver<Post>
+    let errors: Driver<Error>
+
+    // Lifecycle
+
+    init(dependency: Dependency, bindings: Bindings) {
+        let activityIndicator = ActivityIndicator()
+        let errorTracker = ErrorTracker()
+
+        posts = bindings.fetchTrigger
+            .flatMapLatest {
+                return dependency.client.getPosts()
+                    .trackActivity(activityIndicator)
+                    .trackError(errorTracker)
+                    .asDriverOnErrorJustComplete()
+            }
+
+        fetching = activityIndicator.asDriver()
+        errors = errorTracker.asDriver()
+        selectedPost = bindings.selection
+            .withLatestFrom(self.posts) { (indexPath, posts: [Post]) -> Post in
+                return posts[indexPath.row]
+        }
+    }
+
+}
