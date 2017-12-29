0baa3dce0f5e7d0d3c4c08ec4566d58936e39a7b
mgacy <matt@mgacy.com>
2017-12-28 20:17:10 -0700

    Add basic post detail scene
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ MVVMC-SplitViewController/Scenes/Posts/PostDetail/PostDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PostDetailViewController.swift
+//  MVVMC-SplitViewController
+//
+//  Created by Mathew Gacy on 12/28/17.
+//  Copyright © 2017 Mathew Gacy. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+final class PostDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+
+    let disposeBag = DisposeBag()
+    var viewModel: PostDetailViewModel!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        bindViewModel()
+    }
+
+    // MARK: - View Methods
+
+    private func setupView() {
+        title = "Post"
+    }
+
+    func bindViewModel() {
+        viewModel.title
+            .drive(titleLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        viewModel.body
+            .drive(bodyLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+
+}
