5f07348435bc1fec93a77c0d43ea454959bb301a
mgacy <matt@mgacy.com>
2018-01-02 14:59:31 -0700

    Show Login on launch if there is no user
    Signed-off-by: mgacy <matt@mgacy.com>

--- MVVMC-SplitViewController/Application/AppCoordinator.swift
+++ MVVMC-SplitViewController/Application/AppCoordinator.swift
@@ -13,15 +13,35 @@ class AppCoordinator: BaseCoordinator<Void> {
 
     private let window: UIWindow
     private let client: APIClient
+    private let userManager: UserManager
 
     init(window: UIWindow) {
         self.window = window
         self.client = APIClient()
+        self.userManager = UserManager()
     }
 
     override func start() -> Observable<Void> {
-        let tabBarCoordinator = TabBarCoordinator(window: window, client: client)
+        switch userManager.authenticationState {
+        case .signedIn:
+            return showTabBar()
+        case .signedOut:
+            return showLogin()
+                .flatMap { [weak self] result -> Observable<Void> in
+                    guard let strongSelf = self else { return .empty() }
+                    return strongSelf.showTabBar()
+            }
+        }
+    }
+
+    private func showTabBar() -> Observable<Void> {
+        let tabBarCoordinator = TabBarCoordinator(window: self.window, client: self.client)
         return coordinate(to: tabBarCoordinator)
+    }
+
+    private func showLogin() -> Observable<Void> {
+        let loginCoordinator = LoginCoordinator(window: window, client: self.client)
+        return coordinate(to: loginCoordinator)
     }
 
 }
