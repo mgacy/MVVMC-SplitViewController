58bea421a61aefc9ffa31c6888c7b2a932b6870a
mgacy <matt@mgacy.com>
2018-01-08 21:56:11 -0700

    Add UITabBarController subclass
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ MVVMC-SplitViewController/Scenes/Common/TabBarController.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarController.swift
+//  MVVMC-SplitViewController
+//
+//  Created by Mathew Gacy on 1/8/18.
+//  Copyright © 2018 Mathew Gacy. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func collapseSecondaryViewController(_ secondaryViewController: UIViewController, for splitViewController: UISplitViewController) {
+        collapseTabs()
+    }
+
+    override func separateSecondaryViewController(for splitViewController: UISplitViewController) -> UIViewController? {
+        return viewControllers?
+            .flatMap { vc in
+                guard let navController = vc as? NavigationController else { return nil }
+                navController.separate()
+                return vc
+            }
+            .filter { $0 == self.selectedViewController }
+            .first
+    }
+
+    // MARK: -
+
+    func collapseTabs() {
+        guard let vcs = viewControllers else { return }
+        vcs.forEach { viewController in
+            guard let navController = viewController as? NavigationController else {
+                fatalError("\(#function) FAILED : wrong view controller type")
+            }
+            navController.collapse()
+        }
+    }
+
+    func separateTabs() {
+        guard let vcs = viewControllers else { return }
+        vcs.forEach { viewController in
+            guard let navController = viewController as? NavigationController else {
+                fatalError("\(#function) FAILED : wrong view controller type")
+            }
+            navController.separate()
+        }
+    }
+
+}
