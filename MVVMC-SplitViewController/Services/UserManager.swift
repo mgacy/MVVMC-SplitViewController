c58c2f9eba2369052c2b0a3cca609e7a4b5e657b
mgacy <matt@mgacy.com>
2018-01-08 18:26:29 -0700

    Add protocols for authentication-related tasks and
    UserManager extensions conforming to them
    
    Signed-off-by: mgacy <matt@mgacy.com>

--- MVVMC-SplitViewController/Services/UserManager.swift
+++ MVVMC-SplitViewController/Services/UserManager.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Mathew Gacy. All rights reserved.
 //
 
-import Foundation
+import RxCocoa
 
 class UserManager {
 
@@ -17,5 +17,61 @@ class UserManager {
 
     var authenticationState: AuthenticationState = .signedOut
     var currentUser: User? = nil
+
+}
+
+// MARK: - LoginService
+
+protocol LoginService {
+    func login(username: String, password: String) -> Driver<Bool>
+}
+
+extension UserManager: LoginService {
+
+    func login(username: String, password: String) -> Driver<Bool> {
+        let loginResult = arc4random() % 5 == 0 ? false : true
+        return Driver.just(loginResult)
+            .delay(1.0)
+            .do(onNext: { [weak self] _ in
+                self?.authenticationState = .signedIn
+            })
+    }
+
+}
+
+// MARK: - LogoutService
+
+protocol LogoutService {
+    func logout() -> Driver<Bool>
+}
+
+extension UserManager: LogoutService {
+
+    func logout() -> Driver<Bool> {
+        return Driver.just(true)
+            .delay(0.5)
+            .do(onNext: { [weak self] _ in
+                self?.authenticationState = .signedOut
+            })
+    }
+
+}
+
+// MARK: - SignupService
+
+protocol SignupService {
+    func signup(firstName: String, lastName: String, username: String, password: String) -> Driver<Bool>
+}
+
+extension UserManager: SignupService {
+
+    func signup(firstName: String, lastName: String, username: String, password: String) -> Driver<Bool> {
+        let signupResult = arc4random() % 5 == 0 ? false : true
+        return Driver.just(signupResult)
+            .delay(1.0)
+            .do(onNext: { [weak self] _ in
+                self?.authenticationState = .signedIn
+            })
+    }
 
 }
