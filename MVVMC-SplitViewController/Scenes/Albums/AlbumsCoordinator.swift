e8150e979baddf471ea8dab8e136c2a30416e4da
mgacy <matt@mgacy.com>
2018-01-09 22:59:57 -0700

    Add Album coordinator
    Signed-off-by: mgacy <matt@mgacy.com>

---
+++ MVVMC-SplitViewController/Scenes/Albums/AlbumsCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  AlbumsCoordinator.swift
+//  MVVMC-SplitViewController
+//
+//  Created by Mathew Gacy on 1/9/18.
+//  Copyright © 2018 Mathew Gacy. All rights reserved.
+//
+
+import RxSwift
+
+class AlbumsCoordinator: BaseCoordinator<Void> {
+    typealias Dependencies = HasClient
+
+    private let navigationController: UINavigationController
+    private let dependencies: Dependencies
+
+    init(navigationController: UINavigationController, dependencies: Dependencies) {
+        self.navigationController = navigationController
+        self.dependencies = dependencies
+    }
+
+    override func start() -> Observable<Void> {
+
+        // View will never be dismissed
+        return Observable.never()
+    }
+
+    private func showDetailView(with post: Post) {
+        let viewController = PostDetailViewController.instance()
+        viewController.viewModel = PostDetailViewModel(post: post)
+        navigationController.showDetailViewController(viewController, sender: nil)
+    }
+
+}
+
