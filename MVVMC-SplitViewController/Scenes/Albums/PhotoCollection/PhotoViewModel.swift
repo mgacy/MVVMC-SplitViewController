ea763efa0ca97aae9118210a305a1a0cfe65706b
mgacy <matt@mgacy.com>
2018-01-11 20:22:46 -0700

    Extend PhotoViewModel properties
    Signed-off-by: mgacy <matt@mgacy.com>

--- MVVMC-SplitViewController/Scenes/Albums/PhotoCollection/PhotoViewModel.swift
+++ MVVMC-SplitViewController/Scenes/Albums/PhotoCollection/PhotoViewModel.swift
@@ -12,20 +12,35 @@ import RxSwift
 
 final class PhotoViewModel {
 
+    let fetching: Driver<Bool>
+    let errors: Driver<Error>
     //let titleString: String
     let title: Driver<String>
     let thumbnail: Driver<UIImage?>
-    //let image: Driver<UIImage?>
+    let image: Driver<UIImage?>
 
     private let photo: Photo
 
     init(client: APIClient, photo: Photo) {
         self.photo = photo
+
+        let activityIndicator = ActivityIndicator()
+        let errorTracker = ErrorTracker()
+
         //self.titleString = photo.title
         self.title = Driver.just(photo.title)
+
         self.thumbnail = client.getThumbnail(for: photo)
             .asDriverOnErrorJustComplete()
-        //self.image = client.getImage(for: photo)
+
+        self.image = client.getImage(for: photo)
+            .debug("image")
+            .trackActivity(activityIndicator)
+            .trackError(errorTracker)
+            .asDriverOnErrorJustComplete()
+
+        fetching = activityIndicator.asDriver()
+        errors = errorTracker.asDriver()
     }
 
 }
