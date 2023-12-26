f5d18d6973b2c67d0ac827d92bc7649af9fa9c7c
Matt Gacy <matt@mgacy.com>
2023-12-26 15:48:35 -0700

    Use AnyObject

--- MVVMC-SplitViewController/Protocols/ViewModelAttaching.swift
+++ MVVMC-SplitViewController/Protocols/ViewModelAttaching.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RxSwift
 
-protocol ViewModelAttaching: class {
+protocol ViewModelAttaching: AnyObject {
     associatedtype ViewModel: ViewModelType
 
     var bindings: ViewModel.Bindings { get }
